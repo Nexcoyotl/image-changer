fa46616d8c4f87f422462fa3d533e4dd6f994594
Nexcoyotl <nexcoyotlgt@gmail.com>
2016-09-10 22:56:42 -0500

    Generando el nuevo formato de la imagen seleccionada

--- image_changer.sh
+++ image_changer.sh
@@ -7,14 +7,31 @@ echo "Inicia el Script Image Changer";
 echo "Seleccionando archivos";
 
 archivo=$(zenity --file-selection --title="Seleccionador de archivos");
+echo "El archivo que selecciono: $archivo";
 
 #Creación de formulario para seleccionar el formato de salida 
 
-echo "Fromulario para formato de salida";
+echo "Formulario para formato de salida";
 
-formato=$(zenity --list --title="Lista de opciones de formato" --text="Escoje el formato al cual deceas convertir la imagen" --column="Selección" --column="Formatos" --radiolist "1" "png" "2" "jpg" "3" "jpeg" "3" "svg");
+formato=$(zenity --list --title="Lista de opciones de formato" --text="Escoja el formato al cual deseas convertir la imagen" --column="Selección" --column="Formatos" --radiolist "1" "png" "2" "jpg" "3" "jpeg" "3" "svg");
+echo "El formato que selecciono: $formato";
 
-#Creación de Expreción Regular grep para separar la ruta la extención original (png,jpg,jpeg,svg) para despues agregar la nueva
-archivo=$(echo "$archivo" | grep -E -o '[a-zA-Z\/]*[^\.][^a-z]');
+#Creación de Expresión Regular grep para separar la ruta la extensión original (png,jpg,jpeg,svg) para después agregar la nueva extensión
+
+echo "Aplicando Expresión Regular grep para separar la extensión de la imagen";
+
+archivo_er=$(echo "$archivo" | grep -E -o '[a-zA-Z\/]*[^\.][^a-z]');
+echo "El resultado de aplicar la ER: $archivo_er";
  
+#Generando el nuevo formato de la imagen seleccionada
+
+archivo_nuevo=$archivo_er$formato;
+echo "Este sera el nuevo formato $archivo_nuevo";
+
+$(convert $archivo $archivo_nuevo);
+$(chmod 755 $archivo_nuevo);
+
+zenity --info --title="El Resultado de su conversión" --text="$(ls -l $archivo)\n$(ls -l $archivo_nuevo)";
+
+echo "La conversión finalizo con éxito";
 
