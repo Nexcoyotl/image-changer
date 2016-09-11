60070134b5636857cc77cd64afed9501821f890f
Nexcoyotl <nexcoyotlgt@gmail.com>
2016-09-10 21:41:02 -0500

    Creación de seleccionador de formatos

--- image_changer.sh
+++ image_changer.sh
@@ -8,3 +8,11 @@ echo "Seleccionando archivos";
 
 archivos=$(zenity --file-selection --title="Seleccionador de archivos" --multiple);
 
+#Creacion de formulario para seleccionar el formato de salida 
+
+echo "Fromulario para formato de salida";
+
+formato=$(zenity --list --title="Lista de opciones de formato" --text="Escoje el formato al cual deceas convertir la imagen" --column="Selección" --column="Formatos" --radiolist "1" "png" "2" "jpg" "3" "jpeg" "3" "svg");
+
+
+
