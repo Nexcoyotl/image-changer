4eb78345dedb1960bc972e6ff380bd869fec0e12
Nexcoyotl <nexcoyotlgt@gmail.com>
2016-09-10 22:37:37 -0500

    Creación de Expresión Regular grep para separar la ruta la extensión original (png,jpg,jpeg,svg) para después agregar la nueva extensión

--- image_changer.sh
+++ image_changer.sh
@@ -6,13 +6,15 @@ echo "Inicia el Script Image Changer";
 
 echo "Seleccionando archivos";
 
-archivos=$(zenity --file-selection --title="Seleccionador de archivos" --multiple);
+archivo=$(zenity --file-selection --title="Seleccionador de archivos");
 
-#Creacion de formulario para seleccionar el formato de salida 
+#Creación de formulario para seleccionar el formato de salida 
 
 echo "Fromulario para formato de salida";
 
 formato=$(zenity --list --title="Lista de opciones de formato" --text="Escoje el formato al cual deceas convertir la imagen" --column="Selección" --column="Formatos" --radiolist "1" "png" "2" "jpg" "3" "jpeg" "3" "svg");
 
-
+#Creación de Expreción Regular grep para separar la ruta la extención original (png,jpg,jpeg,svg) para despues agregar la nueva
+archivo=$(echo "$archivo" | grep -E -o '[a-zA-Z\/]*[^\.][^a-z]');
+ 
 
